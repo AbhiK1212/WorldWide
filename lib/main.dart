65a9b01cbd0a0ee45b1f3456df1fd6bb2a5557e7
AbhiK1212 <abhiramks05@gmail.com>
2024-08-21 19:31:08 -0500

    Created routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-
-// Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:world_wide/routes.dart';
+import 'package:world_wide/theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
 
-/// We are using a StatefulWidget such that we only create the [Future] once,
-/// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
 class App extends StatefulWidget {
   const App({super.key});
 
@@ -21,28 +16,37 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      // Initialize FlutterFire:
       future: _initialization,
       builder: (context, snapshot) {
-        // Check for errors
         if (snapshot.hasError) {
-          return Text('error');
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ),
+          );
         }
 
-        // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp();
+          return MaterialApp(
+            routes: appRoutes,
+            theme: appTheme,
+          );
         }
 
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Text('loading');
+        return const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Text('Loading...'),
+            ),
+          ),
+        );
       },
     );
   }
