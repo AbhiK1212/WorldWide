88f626f6cbd485b3d41a72f51a973b18a538157a
AbhiK1212 <abhiramks05@gmail.com>
2024-09-03 10:25:27 -0400

    added firestore database, JSON Serializable, etc...

---
+++ lib/services/firestore.dart
@@ -0,0 +1,5 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:world_wide/services/auth.dart';
+import 'package:world_wide/services/models.dart';
